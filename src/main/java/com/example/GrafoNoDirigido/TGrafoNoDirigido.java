67a2810e5e84a8e036e624c38590f9a9184443f6
Tomas <tomasramaviana@gmail.com>
2024-11-23 01:33:12 -0300

    agergado la forma de enviar un paquete mediante arista, agregado logica de retry al envio de paquetes, agregado calcularErrorRate de la arista, agregado esqueleto de metodo para calcular camino mas seguro con sugerencias en comentarios

--- src/main/java/com/example/GrafoNoDirigido/TGrafoNoDirigido.java
+++ src/main/java/com/example/GrafoNoDirigido/TGrafoNoDirigido.java
@@ -173,4 +173,19 @@ public class TGrafoNoDirigido extends TGrafoDirigido implements IGrafoNoDirigido
         return distancias;
     }
 
+    //TODO: definir que devuelve si lsita de aristas o nodos, o ambas.
+    //Este metodo se ejecutara en un metodo main o algo por el estilo
+    //entonces antes de ejecutarlo correr un floydwarshall para ya
+    //tener el camino mas optimo y los valores de todos los otros. *a chequear igual*
+    public void caminoMasSeguro(){
+        //Obtener camino optimo planteado por el floydwarshall ejecutado inicialemnte
+
+        //enviar el paquete a traves del camino optimo (el obtenido anteriormente)
+        //Aca es donde se puede dar que el enviarPaquete devuelva false y ahi replantear
+        //buscar otro camino
+
+        //LLegados a este punto el paquete llegó al nodo destino ya sea a traves de el camino
+        //optimo inicial o el encontrado si es que se cayo algun nodo
+    }
+
 }
